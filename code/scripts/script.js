fb2df25bfd522dd3ad460c3cf67190688357da08
leonard-9500 <leonard.michel@outlook.de>
2021-07-25 10:49:46 +0200

    Tried more 3d.

--- code/scripts/script.js
+++ code/scripts/script.js
@@ -51,7 +51,6 @@ class Player
                    1,  1, -1,
                    1, -1, -1,
                   -1, -1, -1];
-        this.edges = [];
         // Front, left, back, right, top, bottom face.
         this.quads = [0, 1, 2, 3,
                       0, 3, 7, 4,
@@ -59,10 +58,9 @@ class Player
                       5, 6, 2, 1,
                       4, 5, 1, 0,
                       7, 6, 2, 3];
-        // The points in screen space
-        this.pointsSP = [];
         // The player's transformation matrix.
-        let mat = [];
+        this.mat = [];
+        this.ang = 0;
     }
 
     getInput()
@@ -71,50 +69,34 @@ class Player
 
     update()
     {
-        let ang = 30* (Math.PI / 180);
-        let s = Math.sin(ang);
-        let c = Math.cos(ang);
+        this.getInput()
 
-        this.draw(points, quads, m);
+        this.ang += 1 * (Math.PI / 180);
+        let s = Math.sin(this.ang);
+        let c = Math.cos(this.ang);
+        this.mat = [c, 0, -s, 0,
+                    0, 1,  0, 0,
+                    s, 0,  c, 4];
 
-        for (let i = 0; i < this.points.length/3; i++)
-        {
-            this.pointsSP[i*3] = this.points[i*3]/this.points[i*3 + 2];
-            this.pointsSP[i * 3 + 1] = this.points[i * 3 + 1] / this.points[i * 3 + 2];
-            this.pointsSP[i*3 + 2] = 1;
-        }
-        console.log(this.pointsSP);
+        ctx.clearRect(0, 0, SCREEN_WIDTH, SCREEN_HEIGHT);
+        this.draw(this.points, this.quads, this.mat);
+        console.log("finished drawing\n\n\n\n\n\n\n\n");
     }
 
-    draw(ps, ts, m)
+    draw(ps, qs, m)
     {
-        for (let i = 0; i < ts.length; i += 3)
+        for (let i = 0; i < qs.length; i += 4)
         {
-            let p0 = ts[i]*3, p1 = ts[i+1]*3, p2 = ts[i+2]*3;
-            let a = vertexShader(ps[p0], ps[p0 + 1], ps[p0 + 2], m);
-            let b = vertexShader(ps[p1], ps[p1 + 1], ps[p1 + 2], m);
-            let c = vertexShader(ps[p2], ps[p2 + 1], ps[p2 + 2], m);
-            fragmentShader(a, b, c);
+            let p0 = qs[i    ] * 4,
+                p1 = qs[i + 1] * 4,
+                p2 = qs[i + 2] * 4,
+                p3 = qs[i + 3] * 4;
+            let a = this.vertexShader(ps[p0], ps[p0 + 1], ps[p0 + 2], m);
+            let b = this.vertexShader(ps[p1], ps[p1 + 1], ps[p1 + 2], m);
+            let c = this.vertexShader(ps[p2], ps[p2 + 1], ps[p2 + 2], m);
+            let d = this.vertexShader(ps[p3], ps[p3 + 1], ps[p3 + 2], m);
+            this.fragmentShader(a, b, c, d);
         }
-        // Draw faces
-        /*
-        for (let i = 0; i < this.faces.length/4; i++)
-        {
-            ctx.beginPath();
-            ctx.moveTo(this.pointsSP[this.faces[i]     * i] * 100, this.pointsSP[this.faces[i]     * i + 1] * 100);
-            ctx.lineTo(this.pointsSP[this.faces[i + 1] * i] * 100, this.pointsSP[this.faces[i + 1] * i + 1] * 100);
-            ctx.lineTo(this.pointsSP[this.faces[i + 2] * i] * 100, this.pointsSP[this.faces[i + 2] * i + 1] * 100);
-            ctx.lineTo(this.pointsSP[this.faces[i + 3] * i] * 100, this.pointsSP[this.faces[i + 3] * i + 1] * 100);
-            ctx.lineTo(this.pointsSP[this.faces[i]     * i] * 100, this.pointsSP[this.faces[i    ] * i + 1] * 100);
-            ctx.stroke();
-        }
-        */
-        /* draw points
-        for (let i = 0; i < this.points.length/3; i++)
-        {
-            ctx.fillRect(this.pointsSP[i*3]*100, this.pointsSP[i*3+1]*100, 8, 8);
-        }
-        */
     }
 
     vertexShader(x, y, z, m)
@@ -122,21 +104,24 @@ class Player
         var x0 = m[0] * x + m[1] * y + m[ 2] * z + m[ 3];
         var y0 = m[4] * x + m[5] * y + m[ 6] * z + m[ 7];
         var z0 = m[8] * x + m[9] * y + m[10] * z + m[11];
-        return x0, y0, z0;
+        return [x0, y0, z0];
     }
 
-    fragmentShader(a, b, c)
+    fragmentShader(a, b, c, d)
     {
-        let x0 = 200 + 300 * a[0] / a[2], y0 = 150 + 300 * a[1] / a[2];
-        let x1 = 200 + 300 * b[0] / b[2], y1 = 150 + 300 * b[1] / b[2];
-        let x2 = 200 + 300 * c[0] / c[2], y2 = 150 + 300 * c[1] / c[2];
+        let x0 = 100 + 300 * a[0] / a[2], y0 = -100 + 300 * a[1] / a[2];
+        let x1 = 100 + 300 * b[0] / b[2], y1 = -100 + 300 * b[1] / b[2];
+        let x2 = 100 + 300 * c[0] / c[2], y2 = -100 + 300 * c[1] / c[2];
+        let x3 = 100 + 300 * d[0] / d[2], y3 = -100 + 300 * d[1] / d[2];
 
         ctx.beginPath();
         ctx.moveTo(x0, y0);
         ctx.lineTo(x1, y1);
         ctx.lineTo(x2, y2);
+        ctx.lineTo(x3, y3);
         ctx.lineTo(x0, y0);
         ctx.stroke();
+        console.log("fragment drawn\n");
     }
 }
 
@@ -162,9 +147,7 @@ window.main = function()
     //console.log("elapsedTime:" + elapsedTime + "\n");
     tp1 = tp2;
 
-    player.getInput();
     player.update();
-    player.draw();
 }
 
 // Start the game loop
