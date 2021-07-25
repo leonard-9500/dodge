f3989d4d4302cf9118bbc1396e4f39179510eb69
leonard-9500 <leonard.michel@outlook.de>
2021-07-25 09:16:30 +0200

    Add project files.

---
+++ code/scripts/script.js
@@ -0,0 +1,10 @@
+/* Program: script.js
+ * Programmer: Leonard Michel
+ * Start Date: 31.12.2020
+ * Last Change:
+ * End Date: /
+ * License: /
+ * Version: 0.0.0.0
+*/
+
+/**** INITIALIZATION ****/
